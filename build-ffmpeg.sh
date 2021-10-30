a5a47c04cc23c55b5b61012850bc6b3d1f8b684c
Satesh Persaud <34101576+sateshpersaud@users.noreply.github.com>
2021-10-30 16:40:20 -0400

    Update build-ffmpeg.sh

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -16,6 +16,8 @@ apt-get --assume-yes --no-install-recommends --quiet install \
   build-essential \
   cmake \
   git-core \
+  libass-dev \
+  libfreetype6-dev \
   libgnutls28-dev \
   libtool \
   meson \
@@ -32,27 +34,10 @@ mkdir --parents /opt/ffmpeg/sources /opt/ffmpeg/build /opt/ffmpeg/bin
 export PATH="/opt/ffmpeg/bin:$PATH"
 export PKG_CONFIG_PATH=/opt/ffmpeg/build/lib/pkgconfig
 
-cd /opt/ffmpeg/sources
-git -C nasm pull 2>/dev/null || git clone --depth 1 https://github.com/netwide-assembler/nasm.git
-cd nasm
-./autogen.sh
-PATH="/opt/ffmpeg/bin:$PATH"
-./configure --prefix=/opt/ffmpeg/build --bindir=/opt/ffmpeg/bin
-make
-make install
-
-cd /opt/ffmpeg/sources
-git -C x264 pull 2>/dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git
-cd x264
-./configure \
-  --prefix=/opt/ffmpeg/build \
-  --bindir=/opt/ffmpeg/bin \
-  --enable-static \
-  --enable-pic \
-  --disable-cli \
-  --disable-bashcompletion \
-  --disable-interlaced \
-  --bit-depth=8 \
-  --chroma-format=420
-make
+cd /opt/ffmpeg/sources && \
+git -C nasm pull 2>/dev/null || git clone --depth 1 https://github.com/netwide-assembler/nasm.git && \
+cd nasm && \
+./autogen.sh && \
+PATH="/opt/ffmpeg/bin:$PATH" ./configure --prefix=/opt/ffmpeg/build --bindir=/opt/ffmpeg/bin && \
+make && \
 make install
